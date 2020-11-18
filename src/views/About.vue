86ac2272801890c1bea85abe13736806dd282f5d
DanielGara <yo@danielgara.com>
2020-11-18 17:00:32 -0500

    First version

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <div><h1>VariaMos</h1></div>
+    <div>Modeling tool</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+export default class About extends Vue {}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="VariaMos logo" id="logo" src="@/assets/logo.png">
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+export default class Home extends Vue {}
+</script>
+
+<style>
+#logo{
+  max-width: 400px;
+}
+</style>
