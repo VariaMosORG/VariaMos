435f2c984f7944e54f0abce25c5140a45492f44a
DanielGara <yo@danielgara.com>
2020-11-20 19:20:05 -0500

    project label

--- src/views/ProjectModels.vue
+++ src/views/ProjectModels.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card shadow mb-4">
       <div class="card-header py-3">
-          <h6 class="m-0 font-weight-bold text-primary">Project 1</h6>
+          <h6 class="m-0 font-weight-bold text-primary">Project 1 - {{ modelType }}</h6>
       </div>
       <div class="card-body">
         <ModelArea />
@@ -18,5 +18,12 @@ import ModelArea from '@/components/ModelArea.vue';
     ModelArea
   }
 })
-export default class ProjectModels extends Vue {}
+export default class ProjectModels extends Vue {
+  public modelType:any = "";
+
+  public mounted(){
+    this.modelType = String(this.$route.params.modelType);
+    this.modelType = this.modelType.charAt(0).toUpperCase() + this.modelType.slice(1) + "Model";
+  }
+}
 </script>
