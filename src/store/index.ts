e7b843092c12d5d9a72e44af616499599402dec0
DanielGara <yo@danielgara.com>
2020-12-01 16:31:18 -0500

    minor fix store

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,7 +5,7 @@ import { ConfigApp } from './ConfigApp';
 export default createStore({
   state: {
     projects:[] as any,
-    configApp: new ConfigApp("55v","navy","block")
+    configApp: new ConfigApp("400px","navy","flex")
   },
 
   getters: {
