c7187ccd969db987f70ff7811b7c630330145cba
DanielGara <yo@danielgara.com>
2020-12-08 14:33:40 -0500

    remove unused code

--- src/views/Project.vue
+++ src/views/Project.vue
@@ -85,20 +85,6 @@ export default class Project extends mixins(GlobalVueFunctions) {
     pseudoelement.classList.add("dragout");
     pseudoelement.click();
   }
-
-  /*public getBeautyModelName(name:string){
-    if(name.includes("_")){
-      let parts = name.split("_");
-      let completeName = "";
-      for (let i = 0; i < parts.length; i++) {
-        completeName = completeName + parts[i].charAt(0).toUpperCase() + parts[i].slice(1);
-      }
-      completeName = completeName + "Model";
-      return completeName;
-    }else{
-      return name.charAt(0).toUpperCase() + name.slice(1) + "Model";
-    }
-  }*/
 }
 </script>
 
