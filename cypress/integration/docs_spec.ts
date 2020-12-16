dabfe80d5f66242a1368c2c6ccc855c4a030fc1a
DanielGara <yo@danielgara.com>
2020-12-16 08:51:23 -0500

    cypress typescript

--- cypress/integration/docs_spec.ts
+++ cypress/integration/docs_spec.ts
@@ -1,7 +1,8 @@
-describe('Home Test', () => {
-  it('Does not do much!', () => {
+describe('Docs Tests', () => {
+  it('Docs URL test', () => {
     cy.visit('http://localhost:8080');
     cy.get('span').contains('Docs').click();
     cy.url().should('include', '/docs');
-  })
+    cy.get('p').should('contain', 'VariaMos wiki');
+  });
 });
