b5f2ae9f8d6eff7628fe2edc7b288ccd952889e4
DanielGara <yo@danielgara.com>
2021-01-07 07:51:05 -0500

    imp: solver actions

--- src/assets/js/custom_models/feature/FeatureModelActions.vue
+++ src/assets/js/custom_models/feature/FeatureModelActions.vue
@@ -18,6 +18,12 @@
       <a class="dropdown-item dropdown-pointer" v-on:click="importSplotModel">
         Import Splot Model
       </a>
+      <a class="dropdown-item dropdown-pointer" v-on:click="transformModel2Json">
+        Transform Model to JSON
+      </a>
+      <a class="dropdown-item dropdown-pointer" v-on:click="executeSolver">
+        Execute Solver
+      </a>
     </div>
   </div>
 </template>
@@ -25,11 +31,12 @@
 <script lang="ts">
 import { Vue, Options } from 'vue-class-component';
 import { mxgraphFactory } from 'ts-mxgraph';
+import axios from 'axios';
 import { SplotTransformer } from './custom/SplotTransformer';
 
 const {
-  mxImage,
-  mxCellOverlay,
+  mxImage, mxCodec,
+  mxCellOverlay, mxUtils,
 } = mxgraphFactory({ mxLoadResources: false, mxLoadStylesheets: false });
 
 @Options({
@@ -37,6 +44,8 @@ const {
 })
 export default class FeatureModelActions extends Vue {
   public variaMosGraph:any; // VariaMosGraph object
+
+  public customConfig:any; // CustomConfig for feature model
 
   public mounted() {
     if (!this.variaMosGraph.getCustomData().errorCells) {
@@ -46,6 +55,7 @@ export default class FeatureModelActions extends Vue {
       };
       this.variaMosGraph.setCustomData(errorDict); // custom data for this model actions
     }
+    this.customConfig = this.variaMosGraph.configApp.getCustomConfigAsJsonObject().feature;
   }
 
   // clear all the error overlays
@@ -118,6 +128,88 @@ export default class FeatureModelActions extends Vue {
       <br /><br /><textarea class='form-control' id='splotModelCode'></textarea></div>`;
     modal.setData('', 'Splot Model Code', stringBody, 'confirm', confirmAction);
     modal.click();
+  }
+
+  // test transform model to Json
+  public transformModel2Json() {
+    const modal = this.variaMosGraph.getModal();
+    const model = this.variaMosGraph.getModel();
+    const encoder = new mxCodec();
+    const node = encoder.encode(model);
+    const modelData = mxUtils.getPrettyXml(node);
+
+    const headers = {
+      'Content-Type': 'text/plain',
+    };
+
+    axios.post(`${this.customConfig.backendURL}transform/mx-graph-to-json`,
+      modelData,
+      {
+        headers: { headers },
+      })
+      .then((response) => {
+        const JsonCode = JSON.stringify(JSON.parse(response.data), undefined, 2);
+        const stringBody = `<div class='vertical-scroll border'><pre>${JsonCode}</pre></div>`;
+        modal.setData('', 'JSON version code', stringBody);
+        modal.click();
+      })
+      .catch((error) => {
+        modal.setData('error', 'Error', `Wrong backend connection. ${error}`);
+        modal.click();
+      });
+  }
+
+  // execute solver actions
+  public executeSolver() {
+    const modal = this.variaMosGraph.getModal();
+    if (this.customConfig.backendURL != '') {
+      const self = this;
+      const model = this.variaMosGraph.getModel();
+      const encoder = new mxCodec();
+      const node = encoder.encode(model);
+      const modelData = mxUtils.getPrettyXml(node);
+
+      const confirmAction = function anonymousConfirm() {
+        const arraySelections = [];
+        const checkboxes = document.querySelectorAll('input[type=checkbox]:checked') as any;
+
+        for (let i = 0; i < checkboxes.length; i += 1) {
+          arraySelections.push(checkboxes[i].value);
+        }
+        axios.post(`${self.customConfig.backendURL}transform/execute-solver`, {
+          modelData,
+          selectedActions: arraySelections,
+        })
+          .then((response) => {
+            modal.setData('success', 'Success', response.data);
+          })
+          .catch((error) => {
+            modal.setData('error', 'Error', `Wrong backend connection. ${error}`);
+          });
+      };
+
+      const stringBody = `<div>Click confirm button to execute the solver process</div>
+      <div class="form-check">
+        <input class="form-check-input" type="checkbox" name="type" value="dead" />
+        <label class="form-check-label">Get Dead Features</label><br />
+        <input class="form-check-input" type="checkbox" name="type" value="optional" />
+        <label class="form-check-label">Get Fake Optional Features</label><br />
+        <input class="form-check-input" type="checkbox" name="type" value="void" />
+        <label class="form-check-label">Is Void Model?</label><br />
+        <input class="form-check-input" type="checkbox" name="type" value="fake" />
+        <label class="form-check-label">Is Fake Product Line?</label>
+      </div>
+        `;
+      modal.setData(
+        '',
+        'Confirm Solver Execution',
+        stringBody,
+        'confirm',
+        confirmAction,
+      );
+      modal.setSecondaryMessage(true);
+      modal.click();
+    }
   }
 }
 </script>
